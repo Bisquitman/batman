6ffa2c7efc01c681d7dad0d581ce4cb01ef1eedb
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2022-02-17 14:53:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  centeredSlides: true,
+  loopedSlides: 4,
+});
+
+sliderThumbs.on('click', (swiper) => {
+  swiper.slideTo(swiper.clickedIndex);
+});
+
+const sliderMain = new Swiper('.slider-main', {
+  loop: true,
+  spaceBetween: 20,
+  loopedSlides: 4,
+});
+
+sliderThumbs.controller.control = sliderMain;
+sliderMain.controller.control = sliderThumbs;
